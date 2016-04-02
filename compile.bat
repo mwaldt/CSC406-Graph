49334bb47e5ddde03557f827407376a6632969e7
mwaldt <waldm63@gmail.com>
2016-04-02 17:28:06 -0400

    compile was ignored before

--- compile.bat
+++ compile.bat
@@ -2,5 +2,6 @@ javac -d "classes" base/*.java
 javac -d "classes" -cp "classes" undirected/*.java
 javac -d "classes" -cp "classes" directed/*.java
 javac -d "classes" -cp "classes" tester/*.java
+javac -d "classes" -cp "classes" algorithms/*.java
 
 pause 10000
